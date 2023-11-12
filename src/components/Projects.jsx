dc0c326f2fa300097941d6e806d7631701a79bfa
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-11-12 07:38:39 +0100

    added dropdown to all sections

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,19 @@
+'use client'
+import { useState } from "react";
+
+
 export default function Projects(){
+    const[hidden, setHidden] = useState(true);
     return(
-        <div className="flex justify-between px-10 items-center">
-            <h1 className="dark:text-slate-200 text-3xl md:text-4xl lg:text-5xl py-1 font-bold sm:mx-10 z-10">My Projects</h1>
-            <svg width="80px" height="80px" viewBox="0 0 24 24" className="dark:fill-slate-200" xmlns="http://www.w3.org/2000/svg">
-                <path d="M17 9.5L12 14.5L7 9.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
+        <div>
+            <div className="flex justify-between px-10 items-center">            
+                <h1 className="dark:text-slate-200 text-3xl md:text-4xl lg:text-5xl py-1 font-bold sm:mx-10 z-10">My Projects</h1>
+                <svg onClick = {() => setHidden(!hidden)} width="80px" height="80px" viewBox="0 0 24 24" className={"dark:fill-slate-200 " + (!hidden ? "rotate-180" : "") } xmlns="http://www.w3.org/2000/svg">
+                    <path d="M17 9.5L12 14.5L7 9.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+            <div className={hidden ? "hidden" : ""}> 
+            </div>
         </div>
     );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,12 +2,12 @@
 import { useState } from "react";
 
 export default function Skills(){
-    const[hidden, setHidden] = useState(true)
+    const[hidden, setHidden] = useState(true);
     return(
         <div>
             <div className="flex justify-between px-10 items-center">
                 <h1 className="dark:text-slate-200 text-3xl md:text-4xl lg:text-5xl py-1 font-bold sm:mx-10 z-10">My Skills</h1>
-                <svg onClick = {() => setHidden(!hidden)} width="80px" height="80px" viewBox="0 0 24 24" className="dark:fill-slate-200" xmlns="http://www.w3.org/2000/svg">
+                <svg onClick = {() => setHidden(!hidden)} width="80px" height="80px" viewBox="0 0 24 24" className={"dark:fill-slate-200 " + (!hidden ? "rotate-180" : "") } xmlns="http://www.w3.org/2000/svg">
                     <path d="M17 9.5L12 14.5L7 9.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </div>
