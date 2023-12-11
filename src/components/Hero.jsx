800dddaeb8246f160db2ae458461bf6084093e94
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-12-11 18:41:28 +0100

    completed translations Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import text from "../utils/text.json";
 
 export default function Hero(){     
     return(
@@ -13,10 +14,10 @@ export default function Hero(){
                 />
                 <div className="mt-10 flex flex-col gap-4">
                     <div>
-                        <h1>Nice to meet you!</h1>
-                        <h1>I'm <span className="underline decoration-emerald-400 underline-offset-8 decoration-4">Fabio Bauleo</span></h1>
+                        <h1>{text.hero.greetings.english}</h1>
+                        <h1>{text.hero.im.english} <span className="underline decoration-emerald-400 underline-offset-8 decoration-4">Fabio Bauleo</span></h1>
                     </div>
-                    <h2>I'm a Front end Developer specialized in React</h2> 
+                    <h2>{text.hero.job.english}</h2> 
                     <h3>"Maybe I don't know how to do it yet but I will figure it out!"</h3>
                     <a 
                         className="dark:text-slate-200 mt-20 sm:self-start sm:text-xl sm:mx-10 underline decoration-emerald-400 underline-offset-4 decoration-4" 
