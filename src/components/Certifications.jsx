c4694514fd8ecb532ad7562b957d3750665474ff
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2024-01-01 17:52:25 +0100

    added translation to Certifications

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,21 +1,36 @@
 'use client'
 import { useState } from "react";
 import Image from "next/image";
+import { useLanguage } from "@/contexts/LanguageProvider";
+import text from "../utils/text.json";
 
 export default function Certifications(){
     const[hidden, setHidden] = useState(true);
+    const {language} = useLanguage();
     return(
         <div>
             <div className="flex justify-between px-10 items-center">
-                <h1 className="m-0">Certifications</h1>                
+                <h1 className="m-0">{
+                    language === 'english' ? text.certifications.title.english :
+                    language === 'italiano' ? text.certifications.title.italiano :
+                    text.certifications.title.deutsch
+                }</h1>                
                 <svg onClick = {() => setHidden(!hidden)} width="80px" height="80px" viewBox="0 0 24 24" className={"dark:fill-slate-200 transition ease-in-out delay-150 " + (!hidden ? "rotate-180" : "") } xmlns="http://www.w3.org/2000/svg">
                     <path d="M17 9.5L12 14.5L7 9.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </div>
             <div className={`transition-all delay-150 ease-in-out duration-300 ${hidden ? " max-h-0 opacity-1 scale-0 -translate-y-[20px]" : "max-h-[2000px] opacity-100 scale-100 translate-y-0"}`}> 
                 <div className="flex justify-around px-10 items-center p-1 mb-7">
-                    <p className="frontend">Completed</p>
-                    <p className="backend">In progress</p>
+                    <p className="frontend">{
+                        language === 'english' ? text.certifications.completed.english :
+                        language === 'italiano' ? text.certifications.completed.italiano :
+                        text.certifications.completed.deutsch
+                    }</p>
+                    <p className="backend">{
+                        language === 'english' ? text.certifications.in_progress.english :
+                        language === 'italiano' ? text.certifications.in_progress.italiano :
+                        text.certifications.in_progress.deutsch
+                    }</p>
                 </div>
                 <div className="grid md:grid-cols-3">
                     <Image 
