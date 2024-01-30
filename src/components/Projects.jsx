2d41f16968e472131de4e072ede7844fc8f562b3
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2024-01-30 18:13:57 +0100

    links now open in new tab

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,32 +13,16 @@ export default function Projects(){
                 </svg>
             </div>
             <div className={`transition-all delay-150 ease-in-out duration-300 ${hidden ? " max-h-0 opacity-1 scale-0 -translate-y-[20px]" : "max-h-[2000px] opacity-100 scale-100 translate-y-0"}`}> 
-                <div className="flex justify-around px-10 items-center p-1 mb-7">
-                    <p className="frontend">Completed</p>
-                    <p className="backend">In progress</p>
-                </div>
                 <div className="grid md:grid-cols-3">
-                    <Image 
-                        src="/front_end.png"
-                        width={500}
-                        height={500}
-                        alt="Meta Front End Developer Certification"
-                        className="mx-auto my-3 w-3/4 border-4 border-emerald-400"
-                    />
-                     <Image 
-                        src="/cyber_security.png"
-                        width={500}
-                        height={500}
-                        alt="Google Cyber Security Certification"
-                        className="mx-auto w-3/4 my-3 border-4 border-emerald-400"
-                    />
-                    <Image 
-                        src="/backend_placeholder.jpeg"
-                        width={500}
-                        height={500}
-                        alt="Meta Back End Developer Certification"
-                        className="mx-auto my-3 w-3/4 border-4 border-amber-300" 
-                    />
+                    <a href="https://fabio-bauleo.app/" target="_blank">
+                        <Image 
+                            src="/old-portfolio.png"
+                            width={500}
+                            height={500}
+                            alt="My old portfolio"
+                            className="mx-auto my-3 w-3/4 border-4 border-emerald-400"
+                        />
+                    </a>
                 </div>
 
             </div>
--- src/utils/LanguageMenu.jsx
+++ src/utils/LanguageMenu.jsx
@@ -5,9 +5,9 @@ export default function LanguageMenu(props){
     return(
         <div className="mb-4">
             <ul className={`transition-all delay-150 ease-in-out duration-300 ${!props.isVisible ? "max-h-0 opacity-1 scale-0" : "max-h-[800px] opacity-100 scale-100"}`}>
-                <li className='text-2xl dark:text-slate-200' onClick={() => changeLanguage('english')}>English</li>
-                <li className='text-2xl dark:text-slate-200' onClick={() => changeLanguage('italiano')}>Italiano</li>
-                <li className='text-2xl dark:text-slate-200' onClick={() => changeLanguage('deutsch')}>Deutsch</li>
+                <li className='text-2xl dark:text-slate-200 cursor-pointer' onClick={() => changeLanguage('english')}>English</li>
+                <li className='text-2xl dark:text-slate-200 cursor-pointer' onClick={() => changeLanguage('italiano')}>Italiano</li>
+                <li className='text-2xl dark:text-slate-200 cursor-pointer' onClick={() => changeLanguage('deutsch')}>Deutsch</li>
             </ul>
 
         </div>
