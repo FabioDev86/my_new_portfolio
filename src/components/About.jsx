b6f9555e295febbef93be3a999e13b2ec3bc7eaa
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2024-02-01 19:04:52 +0100

    added about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,26 @@
 'use client'
 import { useState } from "react";
-
+import { useLanguage } from "@/contexts/LanguageProvider";
+import text from "../utils/text.json";
 
 export default function About(){
     const[hidden, setHidden] = useState(true);
+    const {language} = useLanguage();
     return(
         <div>
             <div className="flex justify-between px-10 items-center"> 
-                <h1 className="dark:text-slate-200 text-3xl md:text-4xl lg:text-5xl py-1 font-bold sm:mx-10 z-10">About me</h1>
+                <h1 className="m-0">About me</h1>
                 <svg onClick = {() => setHidden(!hidden)} width="80px" height="80px" viewBox="0 0 24 24" className={"dark:fill-slate-200 " + (!hidden ? "rotate-180" : "") } xmlns="http://www.w3.org/2000/svg">
                     <path d="M17 9.5L12 14.5L7 9.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </div>
-            <div className={hidden ? "hidden" : ""}> 
+            <div className={`transition-all delay-150 ease-in-out duration-300 ${hidden ? " max-h-0 opacity-1 scale-0 -translate-y-[20px]" : "max-h-[2000px] opacity-100 scale-100 translate-y-0"}`}> 
+                <p className="mx-10 sm:text-left">{
+                        language === 'english' ? text.about.english :
+                        language === 'italiano' ? text.about.italiano :
+                        text.about.deutsch
+                    }
+                </p>
             </div>
         </div>
     );
